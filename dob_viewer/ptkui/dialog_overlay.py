8fded85732be2233119146d64c93ef59e6a30ff8
HotOffThe Hamster <hotoffthehamster@gmail.com>
2020-04-07 15:54:40 -0500

    Docs: Include PTK license b/c copied code.

--- dob_viewer/ptkui/dialog_overlay.py
+++ dob_viewer/ptkui/dialog_overlay.py
@@ -15,6 +15,41 @@
 # If you lost the GNU General Public License that ships with this software
 # repository (read the 'LICENSE' file), see <http://www.gnu.org/licenses/>.
 
+# AUTHOR: Parts of this file copied from PTK sources, by Jonathan Slenders.
+#   https://github.com/prompt-toolkit/python-prompt-toolkit
+#     examples/full-screen/text-editor.py
+#   Wholly: class MessageDialog, and function show_dialog_as_float.
+#   Partially: function show_message.
+#
+# LICENSE:
+#   Copyright (c) 2014, Jonathan Slenders
+#   All rights reserved.
+#
+#   Redistribution and use in source and binary forms, with or without modification,
+#   are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice, this
+#     list of conditions and the following disclaimer.
+#
+#   * Redistributions in binary form must reproduce the above copyright notice, this
+#     list of conditions and the following disclaimer in the documentation and/or
+#     other materials provided with the distribution.
+#
+#   * Neither the name of the {organization} nor the names of its
+#     contributors may be used to endorse or promote products derived from
+#     this software without specific prior written permission.
+#
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+#   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+#   ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+#   ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 """"""
 
 from gettext import gettext as _
