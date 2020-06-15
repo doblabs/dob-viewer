a5d7a72502fb140f35ffaacbd5eedb71f15b3ce0
HotOffThe Hamster <hotoffthehamster@gmail.com>
2020-06-15 10:54:24 -0500

    Docs: Update online help screen.

--- dob_viewer/traverser/zone_helpful.py
+++ dob_viewer/traverser/zone_helpful.py
@@ -33,31 +33,35 @@ def render_carousel_help():
 
     # FIXME/2020-04-01: Revisit this. Some commands changed; some were never implemented!
     carousel_help = _(
-        """ ┏━━━━━━━━━ NAVIGATION ━━━━━━━━━┳━━━━ EDITING ━━━━┳━━━━━━━ INTERVAL ━━━━━━━━┓
- ┃ → / ←    Next/Previous Fact  ┃  [e] Edit Fact  ┃   Add/Subtract 1 min.   ┃
- ┃ j / k      Same as → / ←     ┠─────────────────╂─────────────────────────┨
- ┃ ↑ / ↓    Move Cursor Up/Down ┃    Or edit:     ┃  To Start: Shift → / ←  ┃
- ┃ h / l      Same as ↑ / ↓     ┃  [a]  act@gory  ┃  To End:    Ctrl → / ←  ┃
- ┃ PgUp     Move Cursor Up/Down ┃  [t]  tagslist  ┃  To Both:               ┃
- ┃  PgDn      by pageful        ┃  [d]  descript  ┃       Ctrl-Shift → / ←  ┃
- ┣━━━━━━━━━━━━━┳━━━━━━━━━━━┳━━━━┻━━━━━┳━━━━━━━━━━━┻━━━━━━━━━┳━━━━━━━━━━━━━━━┫
- ┃  [?] Read   ┃   Ctrl-S  ┃  Ctrl-Q  ┃  [c-p] Split Fact ½ ┃   [u]   Undo  ┃
- ┃  More Help  ┃    Save   ┃   Exit   ┃  [c-e] Empty Fact   ┃  [c-r]  Redo  ┃
- ┣━━━━━━━━━━━━━┻━━━━━━━━━━━┻━━━━┳━━━━━┻━━━━━━━━━━━┳━━━━━━━━━┻━━━━━━━━━━━━━━━┫
- ┃ [g-g]    Jump to First Fact  ┃ H A M S T E R   ┃    H A M S T E R        ┃
- ┃  [G]     Jump to Final Fact  ┃  H A M S T E R  ┃     H A M S T E R       ┃
- ┠──────────────────────────────╂─────────────────┸─────────────────────────┨
- ┃ [Home]   First line Descript ┃                                           ┃
- ┃ [End]    Bottom of Descript. ┃  dob v.{dob_vers: <34} ┃
- ┣━━━━━━━━━━━━━┳━━━━━━━━━━━━┳━━━┻━━━━━┯━━━━━━━━━┯━━━━━━━━━┳━━━━━━━━━━━━━━━━━┫
- ┃  [?] Close  ┃  [q] Easy  ┃  [c-c]  │  [c-x]  │  [c-v]  ┃   [c-z]  Undo   ┃
- ┃  this Help  ┃    Exit    ┃   Copy  │   Cut   │  Paste  ┃   [c-y]  Redo   ┃
- ┗━━━━━━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━┷━━━━━━━━━┷━━━━━━━━━┻━━━━━━━━━━━━━━━━━┛
-
-        """.format(
-            dob_vers=get_version_dob()[:34],
-        ).rstrip()
-    )
+        """ ┏━━━━━━━━━ NAVIGATION ━━━━━━━━┳━━━━ PROMPTS ━━━━┳━━━━━━━ NUDGE TIME ━━━━━━━┓
+ ┃ → / ←   Next/Previous Fact  ┃ a  edit act@cat ┃ [  Sub 1 min. from end   ┃
+ ┃ j / k     Same as → / ←     ┃ t  edit tags    ┃ ]  Add 1 minute to end   ┃
+ ┃ ↑ / ↓   Move Cursor Up/Down ┣━━━━ $EDITOR ━━━━┫ ,  Sub 1 min. from start ┃
+ ┃ h / l     Same as ↑ / ↓     ┃ d  edit descrip.┃ .  Add 1 minute to start ┃
+ ┃ PgUp    Move Cursor Up/Down ┣━━━━ COPIERS ━━━━┫ {{  Sub 5 mins from end   ┃
+ ┃  PgDn     by pageful        ┃  c-c   a@c+tags ┃ }}  Add 5 mins to end     ┃
+ ┃ Home    Top of Description  ┃  c-v    paste   ┃ <  Sub 5 mins from start ┃
+ ┃  End      Bottom of Desc.   ┃ A c-c  copy a@c ┃ >  Add 5 mins to start   ┃
+ ┣━━━ JUMP JUMP JUMP AROUND ━━━┫ T c-c  cpy tags ┃ Ctrl-Shift →  Nudge both ┃
+ ┃ J / K   Back 1 day / Fwd. 1 ┃ D c-c  cpy desc ┃ Ctrl-Shift ←  Nudge both ┃
+ ┃ [n]J/K  Back/Fwd [n] days   ┣━━━━━━━━━ RELATIVE TIME ADJUSTMENTS ━━━━━━━━┫
+ ┃ f       Jump to First Fact  ┃ +[n]m  Set end [n] mins. after start time  ┃
+ ┃ F       Jump to Final Fact  ┃ +[n]h  Set end [n] hours after start time  ┃
+ ┃ YYYYMMDD f  Jump to date    ┃ -[n]m  Set start [n] mins. before end time ┃
+ ┣━━━━━━━┳━━━━━━━┳━━━━━━┳━━━━━━┫ -[n]h  Set start [n] hours before end time ┃
+ ┃ c-s   ┃ c-q   ┃ c-z  ┃ c-y  ┃ [n][ [n]]  Sub/Add [n] minutes from/to end ┃
+ ┃ Save  ┃ Exit  ┃ Undo ┃ Redo ┃ [n], [n].  Sub/Add [n] mins. from/to start ┃
+ ┣━━━━━━━┻━━━━━━━┻━━━━━━┻━━━━━━┻━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫
+ ┃ c-e  Copy curr. Fact meta onto Active Fact ┃ o  Complete Fact and prompt ┃
+ ┃ V    Use curr. Fact meta to start new Fact ┃      for new Fact act@cat   ┃
+ ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫
+ ┃     Learn more tricks at       ┃  Copyright © 2018-2020 Landon Bouma     ┃
+ ┃   https://dob.readthedocs.io   ┃  dob v.{dob_vers: <32} ┃
+ ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+        """
+    ).format(
+        dob_vers=get_version_dob()[:34],
+    ).rstrip()
     return carousel_help
 
 
